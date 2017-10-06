3bc6a0e9f5cf2426e95116a7db74b2ea2084bb18
Yann Blanc <yann.blanc.38144@gmail.com>
2017-10-06 23:46:48 +0200

    First draft with a random file display from Flickr

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
-/* GET home page. */
+var API_key = '&api_key=18274ea05dee494cc19c039b3588c60f&per_page=10&format=json&nojsoncallback=1';
+var endPoint_URL = 'https://api.flickr.com/services/rest/?method='
+
+var buildDefaultUrl = function (method) {
+    return endPoint_URL+method+API_key;
+};
+
+var buildImageUrl = function (imageObject) {
+    return 'http://farm'
+        +imageObject.farm
+        +'.staticflickr.com/'
+        +imageObject.server
+        +'/'
+        +imageObject.id
+        +'_'
+        +imageObject.secret
+        +'.jpg';
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var options = {
+      method: 'post',
+      body: null, // Javascript object
+      json: true, // Use,If you are sending JSON data
+      url: buildDefaultUrl('flickr.photos.getRecent'),
+    }
+    request(options, function (asyncErr, asyncRes, body) {
+    if (asyncErr) {
+        console.log('Error :', asyncErr)
+        return
+    }
+        console.log(' Body :', JSON.stringify(body))
+        res.render('index', { 
+            imageUrl: buildImageUrl(body.photos.photo[0]), 
+            imageAlt: body.photos.photo[0].title, 
+        });
+    });
 });
 
 module.exports = router;
